17c9cb46b952cb0ae374fba2e0b9cfe219db65d3
Christian Wentling <christianwentling@gmail.com>
2017-06-06 15:41:51 -0400

    working on products api

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,10 @@ module.exports = router;
 
 router.use('/users', require('./users'));
 
+// router.use('/reviews', require('./reviews'));
+
+router.use('/products', require('./products'));
+
 router.use((req, res) => {
   res.status(404).send('Not found');
 });
