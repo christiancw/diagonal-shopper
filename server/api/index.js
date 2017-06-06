a535b41eba634308c830b4484c6a0f043110f47d
Patrick Kim <ptrkkim@gmail.com>
2017-06-06 16:01:35 -0400

    hook up order route to index

--- server/api/index.js
+++ server/api/index.js
@@ -6,6 +6,7 @@ router.use('/users', require('./users'));
 // router.use('/reviews', require('./reviews'));
 
 router.use('/products', require('./products'));
+router.user('/orders', require('./orders'));
 
 router.use((req, res) => {
   res.status(404).send('Not found');
