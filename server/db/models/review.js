945ad841fea827b5ef9e79970e92681814dba69c
Patrick Kim <ptrkkim@gmail.com>
2017-06-06 11:50:43 -0400

    basic foundation for review model

---
+++ server/db/models/review.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+module.exports = db.define('review', {
+  rating: {
+    type: Sequelize.INTEGER, // may want to limit somehow 1-5
+    allowNull: false
+  },
+  date: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false // may want to validate a minimum length
+  }
+});
