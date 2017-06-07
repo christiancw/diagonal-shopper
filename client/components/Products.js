781126e1f42a155e1c9c84af08645ff1f45d71d8
Matthew Connelly <matthew.connelly9@gmail.com>
2017-06-07 15:43:27 -0400

    rendering

--- client/components/Products.js
+++ client/components/Products.js
@@ -5,14 +5,14 @@ import { connect } from 'react-redux';
 
 const Products = ({ products }) => {
   // should get products, selectedCategory??
-
+  console.log(products)
   return (
     <div>
       {/* We can get category/search term from state?? */}
       <h3>Results for ***Category or search term?***</h3>
-      {products.map(product => (
+      {products && products.map(product => (
         // <Product key={product.id} product={product} />
-        `${product.name}`
+        <h4>{product.name}</h4>
       ))}
     </div>
   );
@@ -24,10 +24,11 @@ const Products = ({ products }) => {
 
 // hardcoding selectedCategory until we set up a reducer for it
 // products should be pre-filtered by category before it gets here?
-const mapState = ({ products }) => ({
-  products: products.products,
-  selectedCategory: 'Brooms'
-});
+const mapState = (state) => {
+  console.log(state)
+  return {products: state.products.allProducts}
+  //.filter(p => p.categories.includes(selectedCategory
+};
 
 export default connect(mapState)(Products);
 
