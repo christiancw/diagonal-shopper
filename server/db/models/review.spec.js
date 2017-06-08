1d1d9489ed54f450dd62d62b6d404131e0fc70ba
Jacob Han <jacob.kj.han@gmail.com>
2017-06-08 12:20:51 -0400

    Review Specs

---
+++ server/db/models/review.spec.js
@@ -0,0 +1,24 @@
+const { expect } = require('chai');
+const db = require('../db');
+const Review = db.model('review');
+const Sequelize = require('sequelize');
+
+describe('Review model', () => {
+
+  beforeEach(() => {
+    return db.sync({ force: true });
+  });
+
+  it('has all the right fields', function(){
+    return Review.create({
+    rating: 5,
+    // date: Sequelize.NOW,
+    content: "Wow! What a product!"
+  })
+    .then(review => {
+      expect(review.rating).to.equal(5);
+      // expect(review.date).to.equal(Sequelize.NOW);
+      expect(review.content).to.equal('Wow! What a product!');
+    });
+  });
+});
