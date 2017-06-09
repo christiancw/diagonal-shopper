b724c8fe9c92c0a49ec41e4c5bb192ab2f2a21a5
Jacob Han <jacob.kj.han@gmail.com>
2017-06-09 11:53:41 -0400

    Going to install Material UI as dependencies, and implement into React Components

---
+++ client/components/Product.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+const Product = ({ products }) => {
+  var product = products.selectedProduct;
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+const mapState
