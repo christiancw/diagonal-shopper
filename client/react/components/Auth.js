97fde79b50ebe8d7f2340690e11cc6b3de752df8
Patrick Kim <ptrkkim@gmail.com>
2017-06-12 16:20:54 -0400

    feature: logging in or signing up will redirect you to home page, but NOT if doing so from checkout. can continue checking out

--- client/react/components/Auth.js
+++ client/react/components/Auth.js
@@ -16,15 +16,14 @@ const mapSignup = ({ user }) => ({
 });
 
 const mapDispatch = dispatch => ({
-  handleSubmit (evt) {
+  handleSubmit (evt, noRedirect) {
     evt.preventDefault();
     const formName = evt.target.name; // login or signup
     const email = evt.target.email.value;
     const password = evt.target.password.value;
-    dispatch(auth(email, password, formName));
+    dispatch(auth(email, password, formName, noRedirect));
   }
 });
-
 
 export const Login = connect(mapLogin, mapDispatch)(AuthForm);
 export const Signup = connect(mapSignup, mapDispatch)(AuthForm);
